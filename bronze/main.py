0ce4f13f73af8cd18975eb106d4142384486d24b
DevilsSnare <DevilsSnare>
2023-09-14 11:25:25 +0000

    added container information

--- bronze/main.py
+++ bronze/main.py
@@ -1,13 +1,21 @@
 # Databricks notebook source
-container = 'wetelcobatch'
+container = 'wetelcodump'
 storage_account = 'wetelco'
-key = 'r/8yMbm1tixNemxY34fJMlrpbNeWhZbpqeIPSUOoDq6AL6Crp+Dlh6AAt2mFQd2aQnZUZSZWaDws+ASt7m0F2w=='
+key = 'Z9G6HOyHMR7baJdrbhuE7AY7+LcquhN6SdgkUz9ggfeo4lDSQvESX/SJsVRTX+qZzEZxq+083L7z+ASt0dUiCw=='
 
-mount_location = '/mnt/wetelco/wetelcobatch/bronze'
+mount_location = '/mnt/wetelcodump/'
 dbutils.fs.mount(
     source = f'wasbs://{container}@{storage_account}.blob.core.windows.net',
     mount_point = f'{mount_location}',
     extra_configs = {
-        'fs.azure.account.key.foodwagondatalake.blob.core.windows.net': key
+        f'fs.azure.account.key.{storage_account}.blob.core.windows.net': key
     }
 )
+
+# COMMAND ----------
+
+# MAGIC %fs ls /mnt/wetelcodump/bronze/
+
+# COMMAND ----------
+
+

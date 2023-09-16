65f60af9a9f1a892679c25c0d3213fd18d2415e7
ozaid16 <ozaid16>
2023-09-16 17:53:50 +0000

    Customer Information Data cleaned

--- databricks/silver/silver.py
+++ databricks/silver/silver.py
@@ -5,10 +5,41 @@ import dlt
 
 # COMMAND ----------
 
-from pyspark.sql.functions import col, when, mean
+# MAGIC %md
+# MAGIC ####customer_information data
+
+# COMMAND ----------
+
+@dlt.create_table(
+  comment="The cleaned customer_information, ingested from delta",
+  table_properties={
+    "wetelco_deltaliv.quality": "silver",
+    "pipelines.autoOptimize.managed": "true"
+  }
+)
+def customer_information_clean():
+    customer_information_df = spark.read.format("delta").load("dbfs:/pipelines/f7c91f60-3450-426b-80d0-e890be30ed63/tables/customer_information_raw")
+
+    #convert all columns into lower case
+    customer_information_df = customer_information_df.select([col(column).alias(column.lower()) for column in customer_information_df.columns])
+
+    # Convert bigint customer_phone to string and filter out rows where the number of characters is less than 10
+    customer_information_df = customer_information_df.withColumn("customer_phone_str", col("customer_phone").cast("string"))
+    customer_information_df = customer_information_df.filter(length(col("customer_phone_str")) >= 10)
+    
+    # Drop the temporary customer_phone_str column
+    customer_information_df = customer_information_df.drop("customer_phone_str")
+    
+    return customer_information_df
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Billing Data
+
+# COMMAND ----------
+
 from pyspark.sql.window import Window
-from pyspark.sql import functions as F
-from pyspark.sql.types import StringType
 
 # Define a UDF to calculate the mean of bill_amount for each customer_id
 def calculate_mean_udf(bill_amount_col, customer_id_col):

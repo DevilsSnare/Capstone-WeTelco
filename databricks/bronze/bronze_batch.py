4512766fa43cff2651308d151f1c12f7c8cc22e9
ozaid16 <ozaid16>
2023-09-16 13:51:13 +0000

    Generalized directory of zip file for each team Member

--- databricks/bronze/bronze_batch.py
+++ databricks/bronze/bronze_batch.py
@@ -9,10 +9,26 @@
 
 # COMMAND ----------
 
+ozaid = '/Workspace/Repos/md_1692255888379@npmentorskool.onmicrosoft.com/Capstone-WeTelco/databricks/bronze'
+chetan = '/Workspace/Repos/chetan_1692255825295@npmentorskool.onmicrosoft.com/Capstone-WeTelco/databricks/bronze'
+
+# COMMAND ----------
+
+import os
+if os.path.exists(ozaid):
+    my_directory = ozaid
+elif os.path.exists(chetan):
+    my_directory = chetan
+else:
+    raise ValueError("Neither 'ozaid' nor 'chetan' directory exists.")
+
+# COMMAND ----------
+
 from zipfile import ZipFile
 import shutil
-zip_file_path = '/Workspace/Repos/chetan_1692255825295@npmentorskool.onmicrosoft.com/Capstone-WeTelco/databricks/bronze/ebcca86b-6b55-48c9-8e05-4340d2dafd50_83d04ac6-cb74-4a96-a06a-e0d5442aa126_TelecomZip.zip'
-destination_folder = '/Workspace/Repos/chetan_1692255825295@npmentorskool.onmicrosoft.com/Capstone-WeTelco/databricks/bronze/dump_unzipped/'
+
+zip_file_path = f'{my_directory}/ebcca86b-6b55-48c9-8e05-4340d2dafd50_83d04ac6-cb74-4a96-a06a-e0d5442aa126_TelecomZip.zip'
+destination_folder = f'{my_directory}/dump_unzipped/'
 with ZipFile(zip_file_path, 'r') as zip_ref:
     zip_ref.extractall(destination_folder)
 print(f"File unzipped to {destination_folder}")
@@ -23,7 +39,7 @@ mount_point = "/mnt/wetelcodump"
 
 # COMMAND ----------
 
-base_path = "file:///Workspace/Repos/chetan_1692255825295@npmentorskool.onmicrosoft.com/Capstone-WeTelco/databricks/bronze/dump_unzipped"
+base_path = f"file:///{my_directory}/dump_unzipped"
 def moveToADLS(folder_path):
     for item in dbutils.fs.ls(folder_path):
         if item.isDir():
@@ -35,8 +51,8 @@ moveToADLS(base_path)
 
 # COMMAND ----------
 
-dbutils.fs.rm('file:///Workspace/Repos/chetan_1692255825295@npmentorskool.onmicrosoft.com/Capstone-WeTelco/databricks/bronze/ebcca86b-6b55-48c9-8e05-4340d2dafd50_83d04ac6-cb74-4a96-a06a-e0d5442aa126_TelecomZip.zip')
-dbutils.fs.rm('file:///Workspace/Repos/chetan_1692255825295@npmentorskool.onmicrosoft.com/Capstone-WeTelco/databricks/bronze/dump_unzipped', True)
+dbutils.fs.rm(f'file:///{my_directory}/ebcca86b-6b55-48c9-8e05-4340d2dafd50_83d04ac6-cb74-4a96-a06a-e0d5442aa126_TelecomZip.zip')
+dbutils.fs.rm(f'file:///{my_directory}/dump_unzipped', True)
 
 # COMMAND ----------
 
@@ -108,6 +124,7 @@ def billing_raw():
 )
 def customer_information_raw():
     customer_information = spark.read.format("delta").load(f'{mount_point}/bronze/Customer_information')
+    customer_information = customer_information.withColumnRenamed("system status", "system_status")
     return customer_information
 
 # COMMAND ----------
@@ -162,6 +179,11 @@ def device_information_raw():
 )
 def plans_raw():
     plans = spark.read.format("delta").load(f'{mount_point}/bronze/Plans')
+    # Alias the columns
+    plans = plans.withColumnRenamed("Voice Service", "Voice_service") \
+                 .withColumnRenamed("Mobile Data", "Mobile_data") \
+                 .withColumnRenamed("Spam Detection", "Spam_detection") \
+                 .withColumnRenamed("Fraud Prevention","Fraud_prevention")
     return plans
 
 # COMMAND ----------

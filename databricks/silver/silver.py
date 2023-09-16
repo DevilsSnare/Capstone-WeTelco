540e09db3fca7f66526084de18556f3554f81a8f
ozaid16 <ozaid16>
2023-09-16 18:39:39 +0000

    Almost All Dataset Cleaned

--- databricks/silver/silver.py
+++ databricks/silver/silver.py
@@ -10,6 +10,8 @@ import dlt
 
 # COMMAND ----------
 
+from pyspark.sql.functions import col
+
 @dlt.create_table(
   comment="The cleaned customer_information, ingested from delta",
   table_properties={
@@ -20,7 +22,7 @@ import dlt
 def customer_information_clean():
     customer_information_df = spark.read.format("delta").load("dbfs:/pipelines/f7c91f60-3450-426b-80d0-e890be30ed63/tables/customer_information_raw")
 
-    #convert all columns into lower case
+    # Convert all columns into lower case
     customer_information_df = customer_information_df.select([col(column).alias(column.lower()) for column in customer_information_df.columns])
 
     # Convert bigint customer_phone to string and filter out rows where the number of characters is less than 10
@@ -30,7 +32,11 @@ def customer_information_clean():
     # Drop the temporary customer_phone_str column
     customer_information_df = customer_information_df.drop("customer_phone_str")
     
+    # Remove duplicates
+    customer_information_df = customer_information_df.dropDuplicates()
+    
     return customer_information_df
+
 
 # COMMAND ----------
 
@@ -40,6 +46,7 @@ def customer_information_clean():
 # COMMAND ----------
 
 from pyspark.sql.window import Window
+from pyspark.sql.functions import col, when, mean
 
 # Define a UDF to calculate the mean of bill_amount for each customer_id
 def calculate_mean_udf(bill_amount_col, customer_id_col):
@@ -69,6 +76,9 @@ def billing_clean():
     # Drop the mean_bill_amount column as it's no longer needed
     billing_df = billing_df.drop("mean_bill_amount")
     
+    # Remove duplicates
+    billing_df = billing_df.dropDuplicates()
+    
     return billing_df
 
 
@@ -89,10 +99,14 @@ def billing_clean():
 def plans_clean():
     plans_df = spark.read.format("delta").load("dbfs:/pipelines/f7c91f60-3450-426b-80d0-e890be30ed63/tables/plans_raw")
 
-    #convert all columns into lower case
+    # Convert all columns into lower case
     plans_df = plans_df.select([col(column).alias(column.lower()) for column in plans_df.columns])
     
+    # Remove duplicates
+    plans_df = plans_df.dropDuplicates()
+    
     return plans_df
+
 
 # COMMAND ----------
 
@@ -111,10 +125,14 @@ def plans_clean():
 def customer_rating_clean():
     customer_rating_df = spark.read.format("delta").load("dbfs:/pipelines/f7c91f60-3450-426b-80d0-e890be30ed63/tables/customer_rating_raw")
 
-    #convert all columns into lower case
+    # Convert all columns into lower case
     customer_rating_df = customer_rating_df.select([col(column).alias(column.lower()) for column in customer_rating_df.columns])
     
+    # Remove duplicates
+    customer_rating_df = customer_rating_df.dropDuplicates()
+    
     return customer_rating_df
+
 
 # COMMAND ----------
 
@@ -123,7 +141,7 @@ def customer_rating_clean():
 
 # COMMAND ----------
 
-from pyspark.sql.functions import col, when, first
+from pyspark.sql.functions import col
 from pyspark.sql.window import Window
 
 @dlt.create_table(
@@ -161,6 +179,9 @@ def device_information_clean():
         )
     )
     device_information_df = device_information_df.filter(col("os_vendor").isNotNull())
+    
+    # Remove duplicates
+    device_information_df = device_information_df.dropDuplicates()
     
     return device_information_df
 

aa40780be8ac6b5ef607e2422d7e0e5f9630fc41
ozaid16 <ozaid16>
2023-09-19 12:40:07 +0000

    streaming code changed

--- databricks/bronze/bronze_streaming.py
+++ databricks/bronze/bronze_streaming.py
@@ -4,14 +4,24 @@
 
 # COMMAND ----------
 
+from pyspark.sql.functions import col
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 import dlt
 
 # COMMAND ----------
 
-mount_point = '/mnt/wetelcodump/raw/stream'
-shemaLocation = '/mnt/wetelcodump/raw/stream'
+stream_df = spark.readStream \
+        .format("cloudFiles") \
+                 .option("cloudFiles.format", "parquet") \
+                 .option("cloudFiles.schemaLocation", 
+                                  "dbfs:/mnt/wetelcodump/raw/stream") \
+                 .load(f"dbfs:/mnt/wetelcodump/raw/stream")
+
+# Loop through all columns and cast them to string
+for column_name in stream_df.columns:
+    stream_df = stream_df.withColumn(column_name, col(column_name).cast("string"))
+
 
 # COMMAND ----------
 
@@ -25,19 +35,13 @@ from pyspark.sql.functions import col
   }
 )
 def streaming_data():
-    stream_df = spark.readStream \
-        .format("cloudFiles") \
-                 .option("cloudFiles.format", "parquet") \
-                 .option("cloudFiles.schemaLocation", 
-                                  f"dbfs:{shemaLocation}") \
-                 .load(f"dbfs:{mount_point}")
     
-    column_names = stream_df.columns
-    # Create a new DataFrame with updated column names
+    stream_data = stream_df
+    column_names = stream_data.columns
     for column_name in column_names:
-        stream_df = stream_df.withColumnRenamed(column_name, column_name.replace(" ", "_"))
+        stream_data = stream_data.withColumnRenamed(column_name, column_name.replace(" ", "_"))
         
-    return stream_df
+    return stream_data
 
 
 # COMMAND ----------
